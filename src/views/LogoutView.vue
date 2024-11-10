11c1201d50429b8815b86788b59cc1bc40668bd0
Art-sanguandee <67700975@chonburi.spu.ac.th>
2024-11-10 13:07:40 +0700

    Create LogoutView.vue

---
+++ src/views/LogoutView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="logout">
+    <div class="container">
+    <h1>This is an Logout page</h1>
+    <p>Mr.Somdech Sa-nguandee</p>
+    <button type="button" class="btn btn-dark">Primery</button> &nbsp;
+    <button type="button" class="btn btn-success">Secondary</button>
+
+    </div>
+  </div>
+</template>
