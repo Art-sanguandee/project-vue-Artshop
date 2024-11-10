03284ee29f1b294b10b11d3917764a85bb05a2c6
Art-sanguandee <67700975@chonburi.spu.ac.th>
2024-11-10 13:07:43 +0700

    Create ProductView.vue

---
+++ src/views/ProductView.vue
@@ -0,0 +1,131 @@
+<template lang="">
+<div class="product">
+    <div class="container mt-8 my-5">
+        <div class="text-center"> 
+        <h1>Product catalog</h1>
+        </div>
+        <div class="row row-cols-1 row-cols-4 g-4">
+
+    <div class="col">
+        <div class="card h-100">
+        <img src="../assets/ip2.jpg" class="card-img-top" alt="Product image">
+            <div class="card-body">
+            <h5 class="card-title">Product 1 </h5>
+            <p class="card-text">Description of the product goes here.</p>
+            </div>
+            <div class="card-footer">
+            <small class="card-tect">Price: $20.00</small> &nbsp;
+            <button type="button" class="btn btn-outline-primary"> Add </button>
+            </div>
+        </div>
+    </div>
+
+    <div class="col">
+        <div class="card h-100">
+        <img src="../assets/ip3.jpg" class="card-img-top" alt="Product image">
+            <div class="card-body">
+            <h5 class="card-title">Product 2 </h5>
+            <p class="card-text">Description of the product goes here.</p>
+            </div>
+            <div class="card-footer">
+            <small class="card-tect">Price: $20.00</small> &nbsp;
+            <button type="button" class="btn btn-outline-primary"> Add </button>
+            </div>
+        </div>
+    </div>
+
+    <div class="col">
+        <div class="card h-100">
+        <img src="../assets/ip4.jpg" class="card-img-top" alt="Product image">
+            <div class="card-body">
+            <h5 class="card-title">Product 3 </h5>
+            <p class="card-text">Description of the product goes here.</p>
+            </div>
+            <div class="card-footer">
+            <small class="card-tect">Price: $20.00</small> &nbsp;
+            <button type="button" class="btn btn-outline-primary"> Add </button>
+            </div>
+        </div>
+    </div>
+
+    <div class="col">
+        <div class="card h-100">
+        <img src="../assets/ip5.jpg" class="card-img-top" alt="Product image">
+            <div class="card-body">
+            <h5 class="card-title">Product 4 </h5>
+            <p class="card-text">Description of the product goes here.</p>
+            </div>
+            <div class="card-footer">
+            <small class="card-tect">Price: $20.00</small> &nbsp;
+            <button type="button" class="btn btn-outline-primary"> Add </button>
+            </div>
+        </div>
+    </div>
+
+
+ <div class="col">
+        <div class="card h-100">
+        <img src="../assets/ip6.jpg" class="card-img-top" alt="Product image">
+            <div class="card-body">
+            <h5 class="card-title">Product 5 </h5>
+            <p class="card-text">Description of the product goes here.</p>
+            </div>
+            <div class="card-footer">
+            <small class="card-tect">Price: $20.00</small> &nbsp;
+            <button type="button" class="btn btn-outline-primary"> Add </button>
+            </div>
+        </div>
+    </div>
+
+    <div class="col">
+        <div class="card h-100">
+        <img src="../assets/ip7.jpg" class="card-img-top" alt="Product image">
+            <div class="card-body">
+            <h5 class="card-title">Product 6 </h5>
+            <p class="card-text">Description of the product goes here.</p>
+            </div>
+            <div class="card-footer">
+            <small class="card-tect">Price: $20.00</small> &nbsp;
+            <button type="button" class="btn btn-outline-primary"> Add </button>
+            </div>
+        </div>
+    </div>
+
+    <div class="col">
+        <div class="card h-100">
+        <img src="../assets/ip8.jpg" class="card-img-top" alt="Product image">
+            <div class="card-body">
+            <h5 class="card-title">Product 7 </h5>
+            <p class="card-text">Description of the product goes here.</p>
+            </div>
+            <div class="card-footer">
+            <small class="card-tect">Price: $20.00</small> &nbsp;
+            <button type="button" class="btn btn-outline-primary"> Add </button>
+            </div>
+        </div>
+    </div>
+
+    <div class="col">
+        <div class="card h-100">
+        <img src="../assets/ip9.jpg" class="card-img-top" alt="Product image">
+            <div class="card-body">
+            <h5 class="card-title">Product 8 </h5>
+            <p class="card-text">Description of the product goes here.</p>
+            </div>
+            <div class="card-footer">
+            <small class="card-tect">Price: $20.00</small> &nbsp;
+            <button type="button" class="btn btn-outline-primary"> Add </button>
+            </div>
+        </div>
+    </div>
+
+
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {};
+</script>
+<style lang=""></style>
