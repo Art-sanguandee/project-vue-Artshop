bd310a94b372af639b8711c22f65fefabcbf53ff
Art-sanguandee <67700975@chonburi.spu.ac.th>
2024-11-10 13:07:32 +0700

    Create HomeView.vue

---
+++ src/views/HomeView.vue
@@ -0,0 +1,44 @@
+<template>
+<div class="contrainer">
+  <div class="row">
+  <form>
+  <div class="register">
+
+    <section class="h-100 h-custom" style="background-color: #8fc4b7;">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-lg-8 col-xl-7">
+        <div class="card rounded-3">
+          <img src="../assets/ip1.jpg" class="w-100" style="border-top-left-radius: .3rem; border-top-right-radius: .5rem;" alt="Sample photo">
+          <div class="card-body p-2 p-md-5">
+            <h3 class="mb-4 pb-2 pb-md-0 mb-md-5 px-md-2">iPhone เครื่องใหม่ของคุณมาพร้อมแอปอีกมากมาย</h3>
+
+          
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+    
+  </div>
+</form>
+        </div>
+    </div>
+</template>
+
+
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
